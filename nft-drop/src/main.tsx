65e4d9915a1ce70156ed5b098c97576e45e8dedf
Liquorhands <breyerkevin2019@gmail.com>
2023-08-25 19:12:29 +0200

    1

--- nft-drop/src/main.tsx
+++ nft-drop/src/main.tsx
@@ -4,35 +4,37 @@ import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
 import { Toaster } from "./components/ui/Toaster";
-import { getGasless } from "./utils/getGasless";
-import {
-  biconomyApiIdConst,
-  biconomyApiKeyConst,
-  chainConst,
-  relayerUrlConst,
-} from "./consts/parameters";
+import { CronosBeta } from "@thirdweb-dev/chains";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm"
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 const urlParams = new URL(window.location.toString()).searchParams;
 
-const chain =
-  (urlParams.get("chain") && JSON.parse(String(urlParams.get("chain")))) ||
-  chainConst;
-const relayerUrl = urlParams.get("relayUrl") || relayerUrlConst || "";
-const biconomyApiKey =
-  urlParams.get("biconomyApiKey") || biconomyApiKeyConst || "";
-const biconomyApiId =
-  urlParams.get("biconomyApiId") || biconomyApiIdConst || "";
-const sdkOptions = getGasless(relayerUrl, biconomyApiKey, biconomyApiId);
 
-/* Use this when every embed changes to new embeds
-const network = urlParams.get("network") || "ethereum";
-const activeChain = getChainBySlug(network); */
+
+
+const activeChain = "Cronos";
+const sdk = new ThirdwebSDK(CronosBeta);
+
 
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={chain} sdkOptions={sdkOptions}>
+    <ThirdwebProvider 
+      activeChain={{
+        chainId: 25,
+        rpc: ["https://mainnet.cronoslabs.com/v1/55e37d8975113ae7a44603ef8ce460aa"],
+        nativeCurrency: {
+          decimals: 18,
+          name: "Cronos",
+          symbol: "CRO",
+        },
+        shortName: "CRO",
+        slug: "Cronos",
+        testnet: false,
+        chain: "Cronos Mainnet",
+        name: "Cronos Mainnet",
+      }}>
       <Toaster />
       <App />
     </ThirdwebProvider>
